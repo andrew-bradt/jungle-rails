6171abc1af0846c2a55565890727b54beee89c78
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-06 18:30:34 -0400

    add @num_products and @num_categories instance variables to Dashboard, render in view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def show
-    
+    @num_products = Product.count()
+    @num_categories = Category.count()
   end
 end
