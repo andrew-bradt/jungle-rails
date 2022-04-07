f839318ff5630383cb05b223e5d02fd26d477e97
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-07 12:27:11 -0400

    add create method, user_params private method, and Sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root
+    else
+      redirect_to '/signup'
+    end
   end
+
+  private 
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
 end
