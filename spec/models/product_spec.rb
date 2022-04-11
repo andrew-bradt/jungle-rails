128fc3834a0a8f6fc52e8b4c0fba08fc3f3ed8d4
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-11 15:16:12 -0400

    DRY tests by creating @category instance var before each test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,36 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do 
-
-    it 'saves a product with a name, price, quantity and category' do
+    before(:each) do
       @category = Category.new(name: 'Hipster Furniture')
       @category.save!
+    end
 
+    it 'saves a product with a name, price, quantity and category' do
       @product = @category.products.create(name: 'Hipster Chair', price:10000, quantity: 10000)
       expect(@product.id).to be_present
     end 
 
     it 'validates :name, presence: true' do
-      @category = Category.new(name: 'Hipster Furniture')
-      @category.save!
-
       @product = @category.products.create(name: nil, price: 10000, quantity: 9000)
       expect(@product).to_not be_valid
     end 
 
     it 'validates :quantity, presence: true' do
-      @category = Category.new(name: 'Hipster Furniture')
-      @category.save!
-
       @product = @category.products.create(name: 'Morissey Chair', price: 90000, quantity: nil)
-
       expect(@product).to_not be_valid
     end
 
     it 'validates :quantity, presence: true' do
-      @category = Category.new(name: 'Hipster Furniture')
-      @category.save!
-
       @product = @category.products.create(name: 'Birthday Party Desk', price: 10000, quantity: nil, category: @category)
       expect(@product).to_not be_valid
     end
