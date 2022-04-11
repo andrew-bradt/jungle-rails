7a93a30e8935248898409ccdb57de6ca1f641976
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-11 19:30:57 -0400

    add product_details_spec.rb

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Users can navigate to the product detail page by clicking on a product", type: :feature, js: true do
+   
+  # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    
+
+    # DEBUG / VERIFY
+  end
+
+end
