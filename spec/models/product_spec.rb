1a8010c22760863a4fa572fe76a3e9312b1f206f
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-11 13:06:25 -0400

    add saves a product with a name, price, quantity, and category test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do 
+    it 'saves a product with a name, price, quantity and category' do
+      @category = Category.new(name: 'Hipster Furniture')
+      @product = Product.new(name: 'Hipster Chair', price: 10000, quantity: 9000, category: @category)
+      @product.save!
+      expect(@product.id).to be_present
+    end 
   end
 end
