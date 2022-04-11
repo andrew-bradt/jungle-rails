58d2d5686a8fff3706d93c3e3c9f4c403ab7a28f
Andrew Bradt <andrewjbradt@gmail.com>
2022-04-11 15:59:31 -0400

    add should require an email address test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    it 'should require an email address' do
+      @user = User.new(
+        first_name: 'andrew',
+        last_name: 'bradt',
+        email: nil,
+        password: 'some password',
+        password_confirmation: 'some password'
+      )
+      @user.save
+      expect(@user.id).to_not be_present
+    end
+
     it 'should not create a new user if passwords do not match' do
       @user = User.new(
         first_name: 'andrew',
